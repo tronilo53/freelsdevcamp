2b9084df2a1891e240966e326290254619f2b164
tronilo53 <tronilo53@gmail.com>
2023-07-11 15:50:09 +0200

    Implementando Header en HomComponent. implementadas rutas y modificación de assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'Home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
